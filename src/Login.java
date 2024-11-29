c24ba46de2c3a128358d2c3f7a752cb90d064d42
Malcolm <mstone@rollins.edu>
2024-11-29 17:35:32 -0500

    logiPanel holds user login form info

--- src/Login.java
+++ src/Login.java
@@ -1,5 +1,7 @@
-import java.awt.event.ActionListener;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
@@ -8,16 +10,40 @@ import javax.swing.JTextField;
 public class Login extends JPanel implements ActionListener {
     private final JTextField username = new JTextField();
     private final JPasswordField password = new JPasswordField();
+    private final JButton loginButton = new JButton("Login");
 
     private final JLabel usernameLabel = new JLabel("Username");
     private final JLabel passwordLabel = new JLabel("Password");
 
+    private final JPanel logiPanel = new JPanel();
+
     public Login() {
-        setLayout( new GridLayout(2, 2) );
-        add( usernameLabel );
-        add(username);
-        add( passwordLabel );
-        add(password);
+        logiPanel.setLayout( new GridLayout(2, 2) );
+        logiPanel.add( usernameLabel );
+        logiPanel.add(username);
+        logiPanel.add( passwordLabel );
+        logiPanel.add(password);
+
+        add(logiPanel);
+    }
+
+    public String getUsername() {
+        return username.getText();
+    }
+
+    public String getPassword() {
+        return new String(password.getPassword());
+    }
+
+    public void clear() {
+        username.setText("");
+        password.setText("");
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("Username: " + getUsername());
+        System.out.println("Password: " + getPassword());
     }
 }
 
