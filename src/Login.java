6dd50dfc57a10861e475a130efafb96dabb18950
Malcolm <mstone@rollins.edu>
2024-11-29 17:30:10 -0500

    labels adds labels for username and passwords

--- src/Login.java
+++ src/Login.java
@@ -1,4 +1,6 @@
 import java.awt.event.ActionListener;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
@@ -7,6 +9,15 @@ public class Login extends JPanel implements ActionListener {
     private final JTextField username = new JTextField();
     private final JPasswordField password = new JPasswordField();
 
+    private final JLabel usernameLabel = new JLabel("Username");
+    private final JLabel passwordLabel = new JLabel("Password");
 
+    public Login() {
+        setLayout( new GridLayout(2, 2) );
+        add( usernameLabel );
+        add(username);
+        add( passwordLabel );
+        add(password);
+    }
 }
 
