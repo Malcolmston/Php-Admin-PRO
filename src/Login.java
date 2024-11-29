db0fa3a67e41378a1394c3798cc9a6b7b2bd2413
Malcolm <mstone@rollins.edu>
2024-11-29 17:08:05 -0500

    adds swing this will add the username and password felids to the login panel. This panel will be a type of JPanel and will use on click events

--- src/Login.java
+++ src/Login.java
@@ -1,3 +1,10 @@
-public class Login {
-    
+import java.awt.event.ActionListener;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+public class Login extends JPanel implements ActionListener {
+    private JTextField username;
+    private JPasswordField password;
 }
+
