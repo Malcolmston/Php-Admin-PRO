9df40b1f9273fd44e0f5a2bca05d0f84287f4315
Malcolm <mstone@rollins.edu>
2024-11-30 15:16:58 -0500

    initInformation_schema, initPerformance_schema, and initYours this will get a list of all table in the info and system schemas. It will also get a list of all the db's in the set of data

--- src/Info.java
+++ src/Info.java
@@ -1,3 +1,6 @@
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class Info {
@@ -10,4 +13,68 @@ public class Info {
     private ArrayList<String> tables;
     private ArrayList<String> performance;
     private ArrayList<String> yours;
+
+    Info(Connect connect) {
+        this.connect = connect;
+        initInformation_schema();
+        initPerformance_schema();
+        initYours();
+    }
+
+    private void initInformation_schema() {
+        this.tables = new ArrayList<>();
+
+        try {
+            PreparedStatement stmt = connect.getConnection().prepareStatement(INFO);
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                this.tables.add(rs.getString("TABLE_NAME"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+   
+    private void initPerformance_schema() {
+        this.performance = new ArrayList<>();
+
+        try {
+            PreparedStatement stmt = connect.getConnection().prepareStatement(PERFORMANCE);
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                this.performance.add(rs.getString("TABLE_NAME"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void initYours() {
+        this.yours = new ArrayList<>();
+
+        try {
+            PreparedStatement stmt = connect.getConnection().prepareStatement(YOURS);
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                this.yours.add(rs.getString("schema_name"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ArrayList<String> getPerformance() {
+        return tables;
+    }
+
+    public ArrayList<String> getTables() {
+        return tables;
+    }
+
+    private ArrayList<String> getYourTables() {
+        return yours;
+    }
 }
