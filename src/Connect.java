f60c2f7d2b4678b71d4116908c7a1547e45f7292
Malcolm <mstone@rollins.edu>
2024-11-29 21:42:42 -0500

    get url and connection connect to a mysql hose via the database name, port, and address

--- src/Connect.java
+++ src/Connect.java
@@ -1,3 +1,22 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Connect {
+        // Method to establish database connection
+        public static Connection getConnection(String url, String username, String password) throws SQLException {
+            return DriverManager.getConnection(url, username, password);
+        }
+    
+        private static String getUrl (String database) {
+            return "jdbc:mysql://localhost:3306/" + database;
+        }
+    
+        private static String getUrl (int port, String database) {
+            return "jdbc:mysql://localhost:"+port+"/" + database;
+        }
+    
+        private static String getUrl (String address, int port, String database) {
+            return "jdbc:mysql://"+address+":"+port+"/" + database;
+        }
 
 }
