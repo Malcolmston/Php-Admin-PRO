d9999c4343478afe8033919c0dce1305bfb4aea9
Malcolm <mstone@rollins.edu>
2024-12-01 11:41:48 -0500

    hide root element

--- src/SideNav.java
+++ src/SideNav.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import javax.swing.JPanel;
 import javax.swing.JTree;
 import javax.swing.tree.DefaultMutableTreeNode;
-public class SideNav extends JPanel {
+public class SideNav extends JPanel{
     private final  DefaultMutableTreeNode root = new DefaultMutableTreeNode("Root");
 
     public SideNav(ArrayList<String> information_schema, ArrayList<String> performance, ArrayList<String> tables) {        
@@ -28,7 +28,9 @@ public class SideNav extends JPanel {
 
         JTree tree = new JTree(root);
 
+        tree.setShowsRootHandles(true);
+        tree.setRootVisible(false);
+
         add(tree);
     }
-
 }
