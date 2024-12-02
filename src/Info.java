0c7b7cd5a227ce9e99218aafa45be77710bfdc4f
Malcolm <mstone@rollins.edu>
2024-12-02 12:47:12 -0500

    SYS get the Functions, Procedures, Views, and Tables

--- src/Info.java
+++ src/Info.java
@@ -2,23 +2,33 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Info {
     private static final String INFO = "SELECT * FROM information_schema.TABLES";
     private static final String PERFORMANCE = "SELECT * FROM performance_schema.TABLES";
     private static final String YOURS = "SELECT schema_name FROM information_schema.schemata;";
+    private static final String SYS = "SELECT 'Function' AS object_type, ROUTINE_NAME AS name FROM information_schema.ROUTINES WHERE ROUTINE_SCHEMA = 'sys' AND ROUTINE_TYPE = 'FUNCTION'\n" + //
+                "UNION ALL\n" + //
+                "SELECT 'Procedure' AS object_type, ROUTINE_NAME AS name FROM information_schema.ROUTINES WHERE ROUTINE_SCHEMA = 'sys' AND ROUTINE_TYPE = 'PROCEDURE'\n" + //
+                "UNION ALL\n" + //
+                "SELECT 'View' AS object_type, TABLE_NAME AS name FROM information_schema.VIEWS WHERE TABLE_SCHEMA = 'sys'\n" + //
+                "UNION ALL\n" + //
+                "SELECT 'Table' AS object_type, TABLE_NAME AS name FROM information_schema.TABLES WHERE TABLE_SCHEMA = 'sys';";
 
     private Connect connect;
 
     private ArrayList<String> tables;
     private ArrayList<String> performance;
     private ArrayList<String> yours;
+    private HashMap<String, ArrayList<String>> sys;
 
     Info(Connect connect) {
         this.connect = connect;
         initInformation_schema();
         initPerformance_schema();
         initYours();
+        iniSys();
     }
 
     private void initInformation_schema() {
@@ -66,6 +76,64 @@ public class Info {
         }
     }
 
+    private void setUpMap () {
+        HashMap<String, ArrayList<String>> m = new HashMap<>();
+        m.put("Functions", new ArrayList<>());
+        m.put("Procedures", new ArrayList<>());
+        m.put("Views", new ArrayList<>());
+        m.put("Tables", new ArrayList<>());
+
+        this.sys = m;
+    }
+
+    private void iniSys() {
+        this.setUpMap();
+
+
+        try {
+            PreparedStatement stmt = connect.getConnection().prepareStatement(SYS);
+            ResultSet rs = stmt.executeQuery();
+
+            ArrayList< HashMap<String, String>> list = new ArrayList<>();
+
+
+            while (rs.next()) {
+
+                String item = rs.getString("object_type");
+
+                if( item.equalsIgnoreCase("Function")) {
+                    this.sys.get("Functions").add(rs.getString("name"));
+                } else if (item.equalsIgnoreCase("Procedure")) {
+                    this.sys.get("Procedures").add(rs.getString("name"));
+                } else if (item.equalsIgnoreCase("View")) {
+                    this.sys.get("Views").add(rs.getString("name"));
+                } else if (item.equalsIgnoreCase("Table")) {
+                    this.sys.get("Tables").add(rs.getString("name"));
+                }
+
+          
+            }
+
+            list.forEach(item -> {
+
+                if( item.get("object_type").equalsIgnoreCase("Function")) {
+                    sys.get(0).add(item.get("name"));
+                } else if (item.get("object_type").equalsIgnoreCase("Procedure")) {
+                    sys.get(1).add(item.get("name"));
+                } else if (item.get("object_type").equalsIgnoreCase("View")) {
+                    sys.get(2).add(item.get("name"));
+                } else if (item.get("object_type").equalsIgnoreCase("Table")) {
+                    sys.get(3).add(item.get("name"));
+                }
+            });
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
     public ArrayList<String> getPerformance() {
         return tables;
     }
@@ -76,5 +144,9 @@ public class Info {
 
     public  ArrayList<String> getYourTables() {
         return yours;
+    }
+
+    public HashMap<String,ArrayList<String>> getSys() {
+        return sys;
     }
 }
