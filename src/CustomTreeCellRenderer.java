cfe6c5c2616f8dddf352afc2fcfaf0631bc4be9e
Malcolm <mstone@rollins.edu>
2024-12-02 10:31:59 -0500

    this will set the render for folders

--- src/CustomTreeCellRenderer.java
+++ src/CustomTreeCellRenderer.java
@@ -1,3 +1,28 @@
-public class CustomTreeCellRenderer {
+import java.awt.Component;
+import javax.swing.ImageIcon;
+import javax.swing.JTree;
+import javax.swing.tree.DefaultTreeCellRenderer;
 
+public class CustomTreeCellRenderer extends DefaultTreeCellRenderer {
+    private final ImageIcon folderIcon;
+
+    public CustomTreeCellRenderer(ImageIcon icon) {
+        super();
+        this.folderIcon = icon;
+    }
+
+    @Override
+    public Component getTreeCellRendererComponent(JTree tree, Object value, boolean sel,
+                                                  boolean expanded, boolean leaf, int row,
+                                                  boolean hasFocus) {
+        // Get the default renderer's component
+        Component c = super.getTreeCellRendererComponent(tree, value, sel, expanded, leaf, row, hasFocus);
+
+        // Customize icon for non-leaf nodes (folders)
+        if (!leaf && folderIcon != null) {
+            setIcon(folderIcon);
+        }
+
+        return c;
+    }
 }
